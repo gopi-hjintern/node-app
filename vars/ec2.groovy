9553cab3fed1a8f83bcd768914f9d0cdb8d8d08d
gopi-hjintern <gopi.hjintern@outlook.com>
2024-09-09 19:03:00 +0530

    Update ec2.groovy

--- vars/ec2.groovy
+++ vars/ec2.groovy
@@ -1,26 +1,16 @@
 
 def call(String EC2_USER, String EC2_HOST, String SSH_KEY_PATH) {
-    pipeline {
-        agent any
 
-        stages {
-        stage('Deploy to EC2') {
-            steps {
-                script {
+    script {
                     
-                    sh """
-                    ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${EC2_USER}@${EC2_HOST} << 'EOF'
-                        cd /home/ubuntu/
-                        ls
-                        git clone https://github.com/gopi-hjintern/node-app.git
-                        cd node-app
-                        npm install
-                        pm2 start node-app
-
-                    """
-                }
-            }
-        }
-    }
+        sh """
+        ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${EC2_USER}@${EC2_HOST} << 'EOF'
+        cd /home/ubuntu/
+        ls
+        git clone https://github.com/gopi-hjintern/node-app.git
+        cd node-app
+        npm install
+        pm2 start node-app
+        """
     }
 }
