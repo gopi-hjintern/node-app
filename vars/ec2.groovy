f3d1bdded3bb31fa060d7c5afce23d531a2e1f61
gopi-hjintern <gopi.hjintern@outlook.com>
2024-09-09 18:56:00 +0530

    Update ec2.groovy

--- vars/ec2.groovy
+++ vars/ec2.groovy
@@ -1,24 +1,26 @@
 
-def call(String ec2User, String ec2Host, String sshKeyPath, String appDir) {
+def call(String EC2_USER, String EC2_HOST, String SSH_KEY_PATH) {
     pipeline {
         agent any
 
         stages {
-            stage('Deploy to EC2') {
-                steps {
-                    script {
-                        sh """
-                        scp -i $sshKeyPath app.zip $ec2User@$ec2Host:$appDir
-                        ssh -i $sshKeyPath $ec2User@$ec2Host << EOF
-                        cd $appDir
-                        unzip -o app.zip
+        stage('Deploy to EC2') {
+            steps {
+                script {
+                    
+                    sh """
+                    ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${EC2_USER}@${EC2_HOST} << 'EOF'
+                        cd /home/ubuntu/
+                        ls
+                        git clone https://github.com/gopi-hjintern/node-app.git
+                        cd node-app
                         npm install
-                        pm2 restart app
-                        EOF
-                        """
-                    }
+                        pm2 start node-app
+
+                    """
                 }
             }
         }
+    }
     }
 }
